ae8b22506b39fdf0718779d4e09f3cc4ee060582
Rafael Evangelista <50708291+Rafael-Evan@users.noreply.github.com>
2019-10-09 17:51:32 -0300

    09/10/2019
    09/10/2019

--- MilanCorp.API/Controllers/AniversarianteController.cs
+++ MilanCorp.API/Controllers/AniversarianteController.cs
@@ -4,7 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MilanCorp.Domain.Models;
 using MilanCorp.Repository;
+using OfficeOpenXml;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace MilanCorp.API.Controllers
@@ -40,7 +43,7 @@ namespace MilanCorp.API.Controllers
         [HttpGet("AniversarianteAnoAtual")]
         [AllowAnonymous]
         public async Task<ActionResult> GetAniversarianteAno()
-       {
+        {
             try
             {
                 var results = await _context.Aniversariantes.ToListAsync();
@@ -49,7 +52,7 @@ namespace MilanCorp.API.Controllers
                 {
                     var AnoAtual = DateTime.Now.Year;
                     aniversariante.start = new DateTime(AnoAtual, aniversariante.start.Value.Month, aniversariante.start.Value.Day);
-                  
+
                 }
 
                 return Ok(results);
@@ -66,10 +69,87 @@ namespace MilanCorp.API.Controllers
         public async Task<ActionResult<Aniversariante>> PostAniversariante(Aniversariante aniversariante)
         {
             aniversariante.id = new Guid();
+            aniversariante.ativo = true;
             _context.Add(aniversariante);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetAniversariante", new { id = aniversariante.id }, aniversariante);
+        }
+
+        [HttpPost("ImportarAniversariantes"), DisableRequestSizeLimit]
+        [AllowAnonymous]
+        public async Task<IActionResult> ImportarAniversariantesAsync(IList<IFormFile> files)
+        {
+            var upload = new FileUpload();
+
+            var fullPath = "";
+            try
+            {
+                foreach (var item in Request.Form.Files)
+                {
+                    var file = item;
+                    var filename = Path.ChangeExtension(Guid.NewGuid().ToString(), ".xlsx");
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Resources", filename);
+                    var folderName = Path.Combine("wwwroot/Resources/Aniversariantes");
+
+                    var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+                    fullPath = Path.Combine(pathToSave, filename);
+                    //var dbPath = Path.Combine(folderName, filename); 
+
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                }
+
+                FileInfo fileUp = new FileInfo(Path.Combine(fullPath));
+                string FileName = fileUp.Name.Remove(fileUp.Name.Length - 5, 5);
+
+                byte[] arquivo = System.IO.File.ReadAllBytes(fullPath);
+
+                using (ExcelPackage package = new ExcelPackage(fileUp))
+                {
+                    ExcelWorksheet workSheet = package.Workbook.Worksheets["Aniversariantes"];
+                    int totalRows = workSheet.Dimension.Rows;
+
+                    var listaDeAniversariante = await _context.Aniversariantes.ToListAsync();
+
+                    var ExcelList = new List<Aniversariante>();
+
+                    for (int i = 2; i <= totalRows; i++)
+                    {
+
+                        ExcelList.Add(new Aniversariante
+                        {
+                            id = Guid.NewGuid(),
+                            title = workSheet.Cells[i, 1].Value.ToString(),
+                            start = Convert.ToDateTime(workSheet.Cells[i, 2].Value),
+                            ativo = true
+                        });
+                    }
+
+                    foreach (var aniversariante in listaDeAniversariante)
+                    {
+                        foreach (var itemExcel in ExcelList)
+                        {
+                            if (itemExcel.title.Equals(aniversariante.title) && itemExcel.start.Equals(aniversariante.start))
+                            {
+                                ExcelList.Remove(itemExcel);
+                                break;
+                            }
+                        }
+                    }
+                    _context.Aniversariantes.AddRange(ExcelList);
+                    _context.SaveChanges();
+                }
+
+                return Ok(upload.Id);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
         }
     }
 }
