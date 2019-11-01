4e59fcc703f3e2513e729835097f8a4e19028de2
Rafael Evangelista <50708291+Rafael-Evan@users.noreply.github.com>
2019-11-01 17:03:12 -0300

    01/11/2019
    01/11/2019

--- MilanCorp.Domain/Models/Recebimento.cs
+++ MilanCorp.Domain/Models/Recebimento.cs
@@ -11,7 +11,7 @@ namespace MilanCorp.Domain.Models
         public int UserId { get; set; }
         public string Remetente { get; set; }
         public string NumeroDeRastreamento { get; set; }
-        public int Quantidade { get; set; }
+        public string Quantidade { get; set; }
         public DateTime DataDoRecebimento { get; set; }
         public string RecebidoPor { get; set; }
         public string Status { get; set; }
---
+++ MilanCorp.Repository/Migrations/20191101183252_Recebimentoatttt.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MilanCorp.Repository.Migrations
+{
+    public partial class Recebimentoatttt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Quantidade",
+                table: "Recebimentos",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Quantidade",
+                table: "Recebimentos",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
